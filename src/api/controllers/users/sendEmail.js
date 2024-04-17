873bae1d96b620e6d90d97add63d2f450df56aea
Himanshu <himanshujain5690174@gmail.com>
2024-04-17 11:37:55 +0530

    Put email in env file

--- src/api/controllers/users/sendEmail.js
+++ src/api/controllers/users/sendEmail.js
@@ -6,13 +6,13 @@ const sendEmail = async (email, verificationCode) => {
   let transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
-      user: 'rathoursourabh45@gmail.com',
+      user: process.env.EMAIL,
       pass: process.env.EMAIL_PASSWORD,
     },
-  })
+  });
 
   await transporter.sendMail({
-    from: 'rathoursourabh45@gmail.com',
+    from: process.env.EMAIL,
     to: email,
     subject: 'Email Verification for Mask',
     text: `Your verification code is: ${verificationCode}`,
